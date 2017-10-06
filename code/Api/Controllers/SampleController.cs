61014561d94e74145e504f9aefd3fdf7c03a220a
mohayemin <mohayeminul.islam@gmail.com>
2017-10-06 07:43:55 +0600

    #3. add sample free for all request

---
+++ code/Api/Controllers/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+	[Route("sample")]
+	public class SampleController : Controller
+    {
+		[HttpGet("freeforall")]
+		public IActionResult FreeForAll()
+		{
+			return Ok("accessible without access token");
+		}
+    }
+}
